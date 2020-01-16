906f4b25f0ba95b5275b36a042dc98f343c2ed71
Alex Hoang <alexhoang@Alexs-MacBook-Air-2.local>
2020-01-16 19:27:21 +1100

    final

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Employee_DB_app
+Interfaces that make it easy for non-developers to view and interact with information stored in databases.
+
+* Business Use Case
+As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
+
+* With the app, you will be able to 
+    * Add departments, roles, employees
+
+    * View departments, roles, employees
+
+    * Update employee roles
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "hoangvu1906",
+    password: "",
     database: "Employees_DB"
 });
 
@@ -29,6 +29,7 @@ async function start() {
                     "Add new values to a table",
                     "View a table",
                     "Update values of a table",
+                    "Delete a value of a table",
                     "exit"]
             },
             {
@@ -58,6 +59,10 @@ async function start() {
                 Update(tablechoice);
                 break;
 
+            case "Delete a value of a table":
+                Delete(tablechoice);
+                break;
+
             case "exit":
                 connection.end();
                 break;
@@ -72,7 +77,16 @@ function View(tablechoice) {
     connection.query("SELECT * FROM ??", [tablechoice], function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log("ID: " + res[i].id + " || Name: " + res[i].name);
+            if (tablechoice === "departments"){
+                console.log("ID: " + res[i].id + " || Name: " + res[i].name);
+                console.log("-----");
+            } else if (tablechoice === "roles"){
+                console.log("ID: " + res[i].id + " || Title: " + res[i].title + " || Salary: " + res[i].salary);
+                console.log("-----");
+            } else if (tablechoice === "employee"){
+                console.log("ID: " + res[i].id + " || First Name: " + res[i].first_name + " || Last Name: " + res[i].last_name + " || Role_id: " + res[i].role_id + " || Manager_id: " + res[i].manager_id);
+                console.log("-----");
+            };
         };
         start();
     });
@@ -125,14 +139,158 @@ async function Add(tablechoice) {
 
                 connection.query("INSERT INTO roles SET ?", {title: title, salary: salary, department_id: department_id}, function (err, res) {
                     if (err) throw err;
-                    console.log(title + "has been added successfully with id: " + res);
+                    console.log(title + "has been added successfully with id: " + res.insertID);
                     console.log("======================================================")
                     start();
                 });
+            break;
+            case "employee":
+                    var insertEmployee = await inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'first_name',
+                            message: 'Please insert first name'
+                        },
+                        {
+                            type: 'input',
+                            name: 'last_name',
+                            message: 'Please insert last name'
+                        },
+                        {
+                            type: 'input',
+                            name: 'role_id',
+                            message: 'Please insert role id..........NOTE: Must be a number'
+                        },
+                        {
+                            type: 'input',
+                            name: 'manager_id',
+                            message: 'Please insert manager id..........NOTE: Must be a number'
+                        }
+                    ]);
+    
+                    const { first_name, last_name, role_id, manager_id } = insertEmployee;
+                    
+                    console.log(insertEmployee);
+    
+                    connection.query("INSERT INTO employee SET ?", {first_name: first_name, last_name: last_name, role_id: role_id, manager_id: manager_id}, function (err, res) {
+                        if (err) throw err;
+                        console.log(first_name + last_name + " has been added successfully to employee table");
+                        console.log("======================================================")
+                        start();
+                    });
             break;
         }
 
     } catch (err) {
         console.log(err)
     };
+};
+
+async function Update(tablechoice) {
+    try {
+        switch (tablechoice) {
+            case "departments":
+                var updateDep = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'department_id',
+                        message: 'Please enter id of deparment you want to update'
+                    },
+                    {
+                        type: 'input',
+                        name: 'department_value',
+                        message: 'Please enter a new name'
+                    }
+                ]);
+
+                const { department_id, department_value } = updateDep;
+                
+                connection.query("UPDATE departments SET ? WHERE ?;", [{name: department_value}, {id: department_id}], function (err, res) {
+                    if (err) throw err;
+                    console.log("Name of department has been successfully changed to " + department_value );
+                    console.log("======================================================")
+                    start();
+                });
+            break;
+
+            case "roles":
+                var updateRole = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'role_id',
+                        message: 'Please enter id of role you want to update'
+                    },
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'Please insert a new title'
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'Please insert new salary'
+                    },
+                    {
+                        type: 'input',
+                        name: 'new_department_id',
+                        message: 'Please insert a new department_id..............NOTE: Must be a number'
+                    },
+                ]);
+
+                const {role_id, title, salary, new_department_id } = updateRole;
+                
+                console.log(updateRole);
+
+                connection.query("UPDATE roles SET ?, ?, ? WHERE ?", [{title: title}, {salary: salary}, {department_id: new_department_id}, {id: role_id}], function (err, res) {
+                    if (err) throw err;
+                    console.log("Information of " + title + " has been changed successfully ");
+                    console.log("======================================================")
+                    start();
+                });
+            break;
+            case "employee":
+                    var updateEmployee = await inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'employee_id',
+                            message: 'Please enter id of employee you want to update'
+                        },
+                        {
+                            type: 'input',
+                            name: 'first_name',
+                            message: 'Please insert new first name'
+                        },
+                        {
+                            type: 'input',
+                            name: 'last_name',
+                            message: 'Please insert new last name'
+                        },
+                        {
+                            type: 'input',
+                            name: 'new_role_id',
+                            message: 'Please insert new role id..........NOTE: Must be a number'
+                        },
+                        {
+                            type: 'input',
+                            name: 'manager_id',
+                            message: 'Please insert new manager id..........NOTE: Must be a number'
+                        }
+                    ]);
+    
+                    const { employee_id, first_name, last_name, new_role_id, manager_id } = updateEmployee;
+                    
+                    console.log(updateEmployee);
+    
+                    connection.query("UPDATE employee SET ?, ?, ?, ? WHERE ?", [{first_name: first_name}, {last_name: last_name}, {role_id: new_role_id}, {manager_id: manager_id},{id: employee_id}], function (err, res) {
+                        if (err) throw err;
+                        console.log("Information of " + first_name + last_name + " has been updated successfully");
+                        console.log("======================================================")
+                        start();
+                    });
+            break;
+        }
+
+    } catch (err) {
+        console.log(err)
+    };   
 }
